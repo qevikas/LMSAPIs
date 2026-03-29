18a1de4d61f581deca8a1b2b81c3e43c343d0002
Quarter Engineer <127919759+qevikas@users.noreply.github.com>
2026-03-29 18:14:09 +0530

    Upload Project

---
+++ CoreWebAPI/Entities/DTOs/MainMenuDto.cs
@@ -0,0 +1,11 @@
+﻿namespace CoreWebAPI.Entities.DTOs
+{
+    public class MainMenuDto
+    {
+        public int ID { get; set; }
+        public string? Title { get; set; }
+        public string? Url { get; set; }
+        public string? Icon { get; set; }
+        public short? Priority { get; set; }
+    }
+}
---
+++ CoreWebAPI/Entities/MainMenu.cs
@@ -0,0 +1,11 @@
+﻿namespace CoreWebAPI.Entities
+{
+    public class MainMenu: BaseEntity
+    {
+        public int ID { get; set; }
+        public string? Title { get; set; }
+        public string? Url { get; set; }
+        public string? Icon { get; set; }
+        public short? Priority { get; set; }
+    }
+}
